e938f826f7be2df23511b21763e9b5de0248766d
Karol Cislowski <cislowski.karol@hotmail.com>
2021-07-20 20:58:34 +0200

    products finished - test pass/green

--- products/urls.py
+++ products/urls.py
@@ -1,3 +1,5 @@
+from .views import CategoryView, ProductView, user_get_categories, user_get_products
+from django.conf.urls import url
 from django.urls import path
 from .views import CategoryView, user_get_categories
 from rest_framework.routers import DefaultRouter
@@ -9,4 +11,15 @@ router.register('admin/categories', CategoryView)
 
 urlpatterns = router.urls + [
     path('categories/', user_get_categories),
+]
+
+router = DefaultRouter(trailing_slash=False)
+
+router.register('admin/categories', CategoryView)
+router.register('admin/products', ProductView)
+
+
+urlpatterns = router.urls + [
+    path('categories/', user_get_categories),
+    path('products/', user_get_products)
 ]
