b8c4c354392dd4ba748b0ef99fa7e54b7181a264
Karol Cislowski <cislowski.karol@hotmail.com>
2021-07-20 20:45:39 +0200

    category views

--- products/views.py
+++ products/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
+from rest_framework.response import Response
+from .serializers import *
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
+from rest_framework.decorators import api_view, permission_classes
 
-# Create your views here.
+
+class CategoryView(ModelViewSet):
+    serializer_class = CategorySerializer
+    permission_classes = [IsAdminUser]
+    queryset = Category.objects.all()
+
+
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
+def user_get_categories(query):
+    categories = Category.objects.all()
+    serializer = CategorySerializer(categories, many=True)
+    return Response(serializer.data)
